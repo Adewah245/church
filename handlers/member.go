7ece855202bbf4a2add6449e1da48302d7d9d770
jinalegwu1 <adewahjames@gmail.com>
2026-07-22 16:59:09 +0100

    updating data

--- handlers/member.go
+++ handlers/member.go
@@ -1,1 +1,16 @@
 package handlers
+
+import (
+	"church/render"
+	"log"
+	"net/http"
+)
+
+func MembersHandlers(w http.ResponseWriter, r *http.Request) {
+	log.Println("User Visited Members Page")
+	if err := render.Render(w, "members.html", nil); err != nil {
+		log.Println("Render Can't Load", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+}
--- handlers/register.go
+++ handlers/register.go
@@ -10,7 +10,7 @@ func RegisterHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("User visited Register port")
 	if err := render.Render(w, "register.html", nil); err != nil {
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
-		log.Println(err)
+		log.Println("Render Unable To Load", err)
 		return
 	}
 }
